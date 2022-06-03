662642c071239bc8f4c4f53088fe1bc96ff60eca
Michell Zipitría <mzipi@outlook.com>
2022-06-02 22:57:28 -0300

    agregado navbar

---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+function NavBar() {
+  return (
+    <div>
+      <div>
+        <h1>Zipi gamestore</h1>
+      </div>
+      <div>
+        <nav>
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Catalogo</a></li>
+            <li><a href="#">Deseados</a></li>
+            <li><a href="#">Contacto</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
