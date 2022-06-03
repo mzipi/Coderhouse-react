dbc8ceb79c895626ace9655579b93c2ab8e04223
Michell Zipitría <mzipi@outlook.com>
2022-06-02 22:58:50 -0300

    NavBar movido a components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import NavBar from './NavBar';
+import NavBar from './components/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
