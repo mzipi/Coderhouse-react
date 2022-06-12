87247c2ee57dc5609dcc6c048e34da5a4f86e99d
Michell Zipitría <mzipi@outlook.com>
2022-06-12 01:55:58 -0300

    funciones sumar y restar contador

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
 function ItemCount(props) {
-    const stock = 5;
+    const { stock } = props; 
+    const [ initial, setInitial ] = useState(props.initial);
+
+    const sumar = () => {
+        if (initial < stock) {
+            setInitial(initial + 1);
+        };
+    };
+
+    const restar = () => {
+        if(initial > 0) {
+            setInitial(initial - 1);
+        };
+    };
+
     return(
         <div>
             <div className="d-flex">
-                <p>{stock}</p>
-                <button 
-                    type="button" 
-                    className="btn btn-primary">+</button>
-                <button type="button" className="btn btn-primary">-</button>
+                <p>{initial}</p>
+                <button type="button" className="btn btn-primary" onClick={sumar}>+</button>
+                <button type="button" className="btn btn-primary" onClick={restar}>-</button>
             </div>
             <button type="button" className="btn btn-primary">Agregar al carro</button>
         </div>
