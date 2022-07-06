db5a8a95228a85af99d6d68d682f170eabb15bb6
Michell Zipitría <mzipi@outlook.com>
2022-07-05 21:59:18 -0300

    firebase agregado

--- src/components/items/ItemDetail/ItemDetailContainer.jsx
+++ src/components/items/ItemDetail/ItemDetailContainer.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import ItemDetail from "./ItemDetail";
 import producto from "../../../data/catalogo";
 import { useParams } from "react-router-dom";
+import { db } from "../../../firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 function ItemDetailContainer() {
 
@@ -9,19 +11,32 @@ function ItemDetailContainer() {
     const { id } = useParams();
 
     useEffect(()=>{
+        //const collectionItems = collection(db, "items");
+        //const consulta = getDocs(collectionItems);
+        //consulta.then(i => {
+        //    const mapProducts = i.docs.map(doc => {
+        //        const aux = doc.data();
+        //        aux.id = doc.id;
+        //        return aux;
+        //    })
+        //    setItem(mapProducts);
+        //});
+        //consulta.catch(e => console.log(e));
+
         const detailPromise = new Promise((res)=>{
             setTimeout(()=>{
                 res(producto);
             },100);
         })
         detailPromise.then(i => setItem(i))
+
     },[item]);
 
     if(item.length > 0){
         return(
             <div className="container mt-4">
                 <div className="row g-2">
-                    <ItemDetail item={item[id-1]} />
+                    <ItemDetail item={item/*[id]*/} />
                 </div>
             </div>
         );
--- src/components/items/ItemList/ItemListContainer.jsx
+++ src/components/items/ItemList/ItemListContainer.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import ItemList from "./ItemList";
 import catalogo from "../../../data/catalogo";
 import { useParams } from "react-router-dom";
+import { db } from "../../../firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 function ItemListContainer() {
 
@@ -9,6 +11,18 @@ function ItemListContainer() {
     const { id } = useParams();
 
     useEffect(()=>{
+        //const collectionItems = collection(db, "items");
+        //const consulta = getDocs(collectionItems);
+        //consulta.then(i => {
+        //    const mapProducts = i.docs.map(doc => {
+        //        const aux = doc.data();
+        //        aux.id = doc.id;
+        //        return aux;
+        //    })
+        //    setItems(mapProducts);
+        //});
+        //consulta.catch(e => console.log(e));
+        
         const promesa = new Promise((resolve)=>{
             setTimeout(()=>{
                 resolve(catalogo);
